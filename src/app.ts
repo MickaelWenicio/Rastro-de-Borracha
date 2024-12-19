967d28eb859f1c0b292e14cc43857ecd43368547
Mickael Wenicio <mickael.programador123@gmail.com>
2024-12-19 16:44:41 -0300

    feat: creating express server

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import cors from 'cors';
+import express from 'express';
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
