571b809543f1631ef4d34e210f1951a8e2a64770
Mickael Wenicio <mickael.programador123@gmail.com>
2024-12-31 19:27:58 -0300

    feat: user routes included on app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 import express from 'express';
+import userRouter from "./routes/userRoutes";
 
 dotenv.config();
 
 export const app = express();
 
 app.use(express.json());
-
+app.use('/user', userRouter);
 
