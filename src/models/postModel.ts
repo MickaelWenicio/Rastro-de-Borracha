21400142803c5d8ab16b5273ef774b3bec7cba59
Mickael Wenicio <mickael.programador123@gmail.com>
2024-12-31 10:50:13 -0300

    feat: post model created

---
+++ src/models/postModel.ts
@@ -0,0 +1,39 @@
+class post {
+    private id: number;
+    title: string;
+    authorId: number;
+    createdAt: Date;
+    updatedAt: Date;
+    featuredImg: string;
+    content: string;
+
+    constructor(
+        id: number,
+        title: string,
+        authorId: number,
+        createdAt: Date = new Date(),
+        updatedAt: Date = new Date(),
+        featuredImg: string,
+        content: string
+    ){
+        this.id= id,
+        this.title= title,
+        this.authorId= authorId,
+        this.createdAt= createdAt,
+        this.updatedAt= updatedAt,
+        this.featuredImg= featuredImg,
+        this.content= content
+    }
+
+    //getters
+
+    getId(){
+        return this.id;
+    };
+
+    //setters
+
+    setNewUpdate(newDate: Date){
+        this.updatedAt = newDate;
+    };
+}
